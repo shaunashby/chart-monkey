9e27983fa05ec1789c0957522d50480f5b7e0fd7
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-09-09 22:33:05 +0200

    Add first getter script.

---
+++ bin/create-hist-job-postings.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#____________________________________________________________________
+# File: create-hist-job-postings.rb
+#____________________________________________________________________
+#
+# Author: Shaun Ashby <shaun@ashby.ch>
+# Created: 2014-09-09 22:27:19+0200
+# Revision: $Id$
+# Description: A script to exercise the data manipulation classes.
+#
+# Copyright (C) 2014 Shaun Ashby
+#
+#
+#--------------------------------------------------------------------
+$:.unshift File.expand_path("../../lib", __FILE__)
+
+require 'data_grabber'
