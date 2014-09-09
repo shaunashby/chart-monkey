b50d0d3e8da435e03059f891e774e6b1828dc652
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-09-09 22:12:33 +0200

    Add file headers to auto-generated files.

--- lib/data_grabber.rb
+++ lib/data_grabber.rb
@@ -1,3 +1,16 @@
+#____________________________________________________________________
+# File: data_grabber.rb
+#____________________________________________________________________
+#
+# Author: Shaun Ashby <shaun@ashby.ch>
+# Created: 2014-09-09 22:10:59+0200
+# Revision: $Id$
+# Description: Module for a collection of data-manipulation classes.
+#
+# Copyright (C) 2014 Shaun Ashby
+#
+#
+#--------------------------------------------------------------------
 require "data_grabber/version"
 
 module DataGrabber
--- lib/data_grabber/version.rb
+++ lib/data_grabber/version.rb
@@ -1,3 +1,16 @@
+#____________________________________________________________________
+# File: version.rb
+#____________________________________________________________________
+#
+# Author: Shaun Ashby <shaun@ashby.ch>
+# Created: 2014-09-09 22:09:57+0200
+# Revision: $Id$
+# Description: Version file for Data::Grabber.
+#
+# Copyright (C) 2014 Shaun Ashby
+#
+#
+#--------------------------------------------------------------------
 module DataGrabber
   VERSION = "0.0.1"
 end
