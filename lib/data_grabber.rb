ffeb5050f05bf72f4120ea2f501e1670ddd10c88
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-09-16 12:11:19 +0200

    Move email parser code to separate class.

--- lib/data_grabber.rb
+++ lib/data_grabber.rb
@@ -27,4 +27,38 @@ module DataGrabber
     end
   end
 
+  class SimpleEmailParser
+    require 'mail'
+
+    def initialize(file)
+      @file = DATA_INCOMING_PATH + "/#{file}"
+      @messages = Array::new
+    end
+
+    def parse(debug=false)
+      msg = nil
+
+      # Open and split into individual mails:
+      File.open(@file,'r') do |f|
+        while (line = f.gets)
+          begin
+            if (line.match(/\AFrom /))
+              # Save the email message so they can be accessed externally:
+              @messages << Mail.new(msg) if (msg)
+              msg = ''
+            else
+              msg << line.sub(/^\>From/, 'From')
+            end
+          rescue => err
+            puts "Error: ###### ===> #{line}" if debug
+            puts "Error: #{err}...." if debug
+          end
+        end
+      end
+    end
+
+    attr_accessor :messages
+
+  end
+
 end
